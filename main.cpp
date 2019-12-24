273696961f9efc2c84e963d45206aceced2296e6
ignatescu_s <ignatescustefan@gmail.com>
2019-12-24 19:52:49 +0200

    adaugara citire date dintr-un folder

---
+++ main.cpp
@@ -0,0 +1,42 @@
+
+#include <string>
+#include <iostream>
+#include <dirent.h>
+#include <list>
+using namespace std;
+
+void showlist(list <string> g) 
+{ 
+    list <string> :: iterator it; 
+    for(it = g.begin(); it != g.end(); ++it) 
+        cout << *it<<"\n";
+} 
+list<string> getFileNameForDirectori(const char *filePath)
+{
+    list<string> fileName;
+
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir (filePath)) != NULL) 
+    {
+        /* print all the files and directories within directory */
+        while ((ent = readdir (dir)) != NULL)
+        {
+           // cout<<ent->d_name<<"\n";
+            fileName.push_front(ent->d_name);
+        }
+        closedir (dir);
+    }
+    else
+    {
+        /* could not open directory */
+        perror ("");
+        return fileName;
+    }
+    return fileName;
+}
+int main(int argc,char **argv)
+{
+    list<string> list=getFileNameForDirectori("/home/stefan/Desktop/alpd-MapReduce/dateIntrare");
+    showlist(list);
+}
